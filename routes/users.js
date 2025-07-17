486cbd8cceaff2258d57f181f39e01e8391b8740
Brajesh Kumar <bk117134@gmail.com>
2025-07-18 01:34:09 +0530

    Initial commit - fresh upload

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const uri =
+  'mongodb+srv://princebhatt316:DnFqDYxei3ai8X3g@cluster0.vdwbeya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+  // hggjhjhkjkjk
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log('Mongo Connected');
+  })
+  .catch(() => {
+    console.log('Not Connected');
+  });
+
+const loginSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const collection = new mongoose.model('Collection1', loginSchema);
+module.exports = collection;
